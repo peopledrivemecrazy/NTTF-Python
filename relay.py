75d569819c4f3caa387a77d2d77a6cf908c6a393
Shriji <shrijike@gmail.com>
2019-01-25 19:51:28 +0000

    relay fixes

--- relay.py
+++ relay.py
@@ -4,11 +4,13 @@ import RPi.GPIO as GPIO
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(17, GPIO.OUT)
 
-relay_condition = sys.argv[0] #Capture use input
-
-if relay_condition == 1:
+relay_condition = sys.argv[1] #Capture use input
+print relay_condition
+if relay_condition == "1":
+	print "Relay On"
 	GPIO.output(17, True)
-if relay_condition == 0: 
+if relay_condition == "0":
+        print "Relay Off"
         GPIO.output(17, False)
 
 
